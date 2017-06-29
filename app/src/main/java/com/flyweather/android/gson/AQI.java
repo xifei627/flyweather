a21dae1086a16e251bbe1aceac4d48bd419640a4
xifei <zjwang627@163.com>
2017-06-29 15:18:14 +0800

    完成项目

---
+++ app/src/main/java/com/flyweather/android/gson/AQI.java
@@ -0,0 +1,15 @@
+package com.flyweather.android.gson;
+
+public class AQI {
+
+    public AQICity city;
+
+    public class AQICity {
+
+        public String aqi;
+
+        public String pm25;
+
+    }
+
+}
